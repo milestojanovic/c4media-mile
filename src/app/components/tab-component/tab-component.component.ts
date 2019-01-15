32aded7a968891032bc69c19ffa19ad6c727a5a1
Mile Stojanovic <ms@jystcreative.com>
2019-01-15 15:48:33 +0100

    homepage tabs like component

--- src/app/components/tab-component/tab-component.component.ts
+++ src/app/components/tab-component/tab-component.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab-component.component.scss']
 })
 export class TabComponentComponent implements OnInit {
+  tabs = [
+    {
+      link: 'https://www.infoq.com/about-infoq',
+      title: 'InfoQ Reader',
+      subtitle: 'learn about InfoQ',
+      image: 'InfoQ.svg',
+      icon: 'lnr-arrow-right'
+    },
+    {
+      link: 'https://qconferences.com',
+      title: 'QCon Attendee / Speaker',
+      subtitle: 'learn about InfoQ',
+      image: 'qcon.svg',
+      icon: 'lnr-arrow-right'
+    },
+    {
+      link: '/',
+      title: 'QCon Exhibitor / InfoQ Advertiser',
+      subtitle: 'download file',
+      image: 'icon-file.png',
+      icon: 'lnr-download'
 
+    },
+    {
+      link: '/career',
+      title: 'Careers',
+      subtitle: 'learn about InfoQ',
+      image: 'logo-black.png',
+      icon: 'lnr-arrow-right'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
